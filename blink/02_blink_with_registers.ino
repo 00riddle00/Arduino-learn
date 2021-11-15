62cd3c1b636b063d298d2afe1d7eccab65beff79
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-11-15 16:18:59 +0200

    Restructure (cont.0

--- blink/02_blink_with_registers.ino
+++ blink/02_blink_with_registers.ino
@@ -1,11 +1,13 @@
 const int led_pin = PB5;
 
 void setup() { 
+
   // Set LED pin to be output
   DDRB |= (1 << (led_pin)); // DDRB = xx1xxxxx;
 }
 
 void loop() {
+
   // output either 5V or 0V on led_pin;
   PORTB ^= (1 << led_pin); // PORTB = xx{0,1}xxxxx;
 
